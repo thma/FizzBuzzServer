8463bbf41990b8faf57182088ab539371557848e
Mahler, Thomas <thomas.mahler@ista.com>
2022-11-29 15:48:04 +0100

    Fiss buss works like a charm

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,15 @@
 module Main (main) where
+import System.Environment (getArgs)
 
+fizzBuzz :: Integer -> String
+fizzBuzz n | fizzBuzzCombo n == "" = show n
+           | otherwise             = show n ++ " becomes " ++ fizzBuzzCombo n
+  where
+    fizzBuzzCombo n = fizz n ++ buzz n
+    fizz = anyzz "Fizz" 3
+    buzz = anyzz "Buzz" 5
+    anyzz word factor n | n `mod` factor == 0 = word
+                        | otherwise           = ""
 
 main :: IO ()
-main = putStrLn ("Executable not implemented" :: String)
+main = getArgs >>= putStrLn . fizzBuzz . read . head
